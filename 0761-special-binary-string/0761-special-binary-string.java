98c68e88a9c6511f88bb1d1e7a61a3b934d10be4
RudraPratapSingh <170000869+RudraPratapSingh05@users.noreply.github.com>
2026-02-20 19:32:03 +0530

    Time: 3 ms (51.42%), Space: 42.6 MB (99.76%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Solution {
+    public String makeLargestSpecial(String s) {
+        int count = 0;
+        int i = 0;
+        List<String> res = new ArrayList<>();
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == '1') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                res.add("1" + makeLargestSpecial(s.substring(i + 1, j)) + "0");
+                i = j + 1;
+            }
+        }
+        Collections.sort(res, Collections.reverseOrder());
+        return String.join("", res);
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "11011000";
+        System.out.println("Result: " + sol.makeLargestSpecial(s)); 
+    }
+}
