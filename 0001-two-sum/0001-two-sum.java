351127059dcf6aa4e119331c8bf470fb20ee77ed
RudraPratapSingh <170000869+RudraPratapSingh05@users.noreply.github.com>
2026-05-06 17:42:23 +0530

    Time: 31 ms (41.44%), Space: 47.1 MB (58.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,18 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Map to store: Value -> Index
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int current = nums[i];
-            int complement = target - current;
-            
-            // If the "missing piece" is in the map, we are done!
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            for(int j = i + 1; j < nums.length; j++) {
+
+                if(nums[j] == complement) {
+                    return new int[]{i, j};
+                }
             }
-            
-            // Otherwise, store this number so a future number can find it
-            map.put(current, i);
         }
-        
-        // The problem guarantees a solution, so we don't need a fallback
-        return new int[] {};
+
+        return null;
     }
 }
