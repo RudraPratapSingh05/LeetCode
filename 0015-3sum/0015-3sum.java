d705c437a6f5ce1d02891be87dcedc75ab2ebfda
RudraPratapSingh <170000869+RudraPratapSingh05@users.noreply.github.com>
2026-06-01 02:30:13 +0530

    Time: 35 ms (42.59%), Space: 58.9 MB (84.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,22 +4,21 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         for(int i=0;i<nums.length-2;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-                int left = i+1;
-                int right = nums.length-1;
-                while(left<right){
-                    int total = nums[left] + nums[right] + nums[i];
-                    if(total==0){
-                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                        while(left<right && nums[left]==nums[left+1]) left++;
-                        while(left<right && nums[right]==nums[right-1]) right--;
-                        left++;
-                        right--;
-                    }else if(total<0){
-                        left++;
-                    }else if(total>0){
-                        right--;
-                    }
-                
+            int left = i+1;
+            int right = nums.length-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum==0){
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left<right && nums[left]==nums[left+1]) left++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
+                    left++;
+                    right--;
+                }else if(sum>0) {
+                    right--;
+                }else if(sum<0){
+                    left++;
+                }
             }
         }
         return result;
