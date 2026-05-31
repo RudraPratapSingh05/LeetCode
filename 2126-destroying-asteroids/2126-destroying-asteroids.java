e544d8817f960c99429eb65993c4cc79ecfe6dc6
RudraPratapSingh <170000869+RudraPratapSingh05@users.noreply.github.com>
2026-05-31 13:28:38 +0530

    Time: 27 ms (31.18%), Space: 107.6 MB (16.4%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long currentMass = mass;
+        for(int i=0;i<asteroids.length;i++){
+            if(asteroids[i]<=currentMass){
+                currentMass+=asteroids[i];
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
