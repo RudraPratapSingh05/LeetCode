8a56cb3758ee2a9d4f6d044fae4ee2aa4eb82027
RudraPratapSingh <170000869+RudraPratapSingh05@users.noreply.github.com>
2026-06-08 09:25:31 +0530

    Time: 5 ms (73.56%), Space: 160.3 MB (21.16%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,19 +1,22 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> less = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer> more = new ArrayList<>();
-
-        for(int num:nums){
-            if(num<pivot) less.add(num);
-            else if(num==pivot) equal.add(num);
-            else more.add(num);
-        }
         int[] result = new int[nums.length];
-        int i=0;
-        for(int num:less) result[i++] = num;
-        for(int num:equal) result[i++] = num;
-        for(int num:more) result[i++] = num;
+        int left = 0, right = nums.length - 1;
+        
+        for (int i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+            if (nums[i] < pivot) {
+                result[left] = nums[i];
+                left++;
+            }   
+            if (nums[j] > pivot) {
+                result[right] = nums[j];
+                right--;
+            }
+        }
+        while (left <= right) {
+            result[left] = pivot;
+            left++;
+        }
         return result;
     }
 }
